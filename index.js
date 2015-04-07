170bc479241ef11fb86828be7c915158ca31e239
David Oliveros <dato.oliveros@gmail.com>
2015-04-06 19:15:49 -0500

    Hard-coded the exports in index.js

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-var readdirSync = require('fs').readdirSync;
-var basename = require('path').basename;
 
-var commons = {};
-
-readdirSync(__dirname + '/lib').forEach(function (filename) {
-	var name = basename(filename, '.js');
-	commons[name] = require(__dirname + '/lib/' + name);
-});
-
-module.exports = commons;
+exports.addApostrophe       = require('./lib/addApostrophe');
+exports.buildCommunityKey   = require('./lib/buildCommunityKey');
+exports.buildObject         = require('./lib/buildObject');
+exports.capitalize          = require('./lib/capitalize');
+exports.compactObject       = require('./lib/compactObject');
+exports.cutPath             = require('./lib/cutPath');
+exports.deep                = require('./lib/deep');
+exports.easingPattern       = require('./lib/easingPattern');
+exports.emailRegex          = require('./lib/emailRegex');
+exports.emitter             = require('./lib/emitter');
+exports.emptyFunction       = require('./lib/emptyFunction');
+exports.extractStopwords    = require('./lib/extractStopwords');
+exports.firstWord           = require('./lib/firstWord');
+exports.getHTML             = require('./lib/getHTML');
+exports.getLinks            = require('./lib/getLinks');
+exports.getSignedCookie     = require('./lib/getSignedCookie');
+exports.getTeaser           = require('./lib/getTeaser');
+exports.getTopOffset        = require('./lib/getTopOffset');
+exports.hasSameProperties   = require('./lib/hasSameProperties');
+exports.highlight           = require('./lib/highlight');
+exports.isAdmin             = require('./lib/isAdmin');
+exports.isEmail             = require('./lib/isEmail');
+exports.isModerator         = require('./lib/isModerator');
+exports.isObjectId          = require('./lib/isObjectId');
+exports.isURL               = require('./lib/isURL');
+exports.isUser              = require('./lib/isUser');
+exports.objectIdRegex       = require('./lib/objectIdRegex');
+exports.parseJSON           = require('./lib/parseJSON');
+exports.permute             = require('./lib/permute');
+exports.pluralize           = require('./lib/pluralize');
+exports.pushUnique          = require('./lib/pushUnique');
+exports.randomCharacter     = require('./lib/randomCharacter');
+exports.randomGarbage       = require('./lib/randomGarbage');
+exports.randomString        = require('./lib/randomString');
+exports.removeHTML          = require('./lib/removeHTML');
+exports.removeNewlines      = require('./lib/removeNewlines');
+exports.removeTrailingSlash = require('./lib/removeTrailingSlash');
+exports.sanitizeUrl         = require('./lib/sanitizeUrl');
+exports.smoothScroll        = require('./lib/smoothScroll');
+exports.stopwordsAll        = require('./lib/stopwordsAll');
+exports.stopwordsUrl        = require('./lib/stopwordsUrl');
+exports.urlRegex            = require('./lib/urlRegex');
