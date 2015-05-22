9ac9d8442ff759026b31dad3400a8d4fc7b3d26e
David Oliveros <dato.oliveros@gmail.com>
2015-05-22 13:48:24 -0500

    Added openPopup, Linted codebase, passed tests to ES5

--- test/index.js
+++ test/index.js
@@ -1,19 +1,20 @@
-import common from '..';
-import { expect } from 'chai';
+/*eslint no-unused-expressions:0*/
+var common = require('../index');
+var expect = require('chai').expect;
 
-describe('common', () => {
-	it('require', () => {
+describe('common', function() {
+	it('require', function() {
 		expect(common.addApostrophe).exist;
 		expect(common.buildCommunityKey).exist;
 		expect(common.buildObject).exist;
 	});
 
-	it('require lib', () => {
-		let capitalize = require('../lib/capitalize');
+	it('require lib', function() {
+		var capitalize = require('../lib/capitalize');
 		expect(capitalize).exist;
-	})
+	});
 
-	it('capitalize', () => {
+	it('capitalize', function() {
 		expect(common.capitalize('')).equal('');
 		expect(common.capitalize('antonio')).equal('Antonio');
 	});
