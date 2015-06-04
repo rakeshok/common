cdaaa81427be270fb6b057504253024e45ee2029
David Oliveros <dato.oliveros@gmail.com>
2015-06-04 13:23:21 -0500

    Added a key check on obj2 in hasSameProperties

--- util/hasSameProperties.js
+++ util/hasSameProperties.js
@@ -4,7 +4,12 @@ function hasSameProperties(obj1, obj2) {
 	var same = true;
 
 	Object.keys(obj1).forEach(function(key) {
-		if (!obj2.hasOwnProperty(key) || obj2[key] !== obj1[key])
+		if (!obj2.hasOwnProperty(key) || obj1[key] !== obj2[key])
+			same = false;
+	});
+
+	Object.keys(obj2).forEach(function(key) {
+		if (!obj1.hasOwnProperty(key) || obj1[key] !== obj2[key])
 			same = false;
 	});
 
