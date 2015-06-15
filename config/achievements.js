9c54699ab7a471a736c8d5f4f674d3bb54a8a3d5
AlexKvazos <contact@alexkvazos.com>
2015-06-15 18:08:20 -0500

    Use ES5 only syntax

--- config/achievements.js
+++ config/achievements.js
@@ -6,58 +6,58 @@ module.exports = {
 
 	opVision: {
 		rep: 10,
-		name: `OP Vision`,
+		name: 'OP Vision',
 		icon: '/images/placeholders/rep-10.svg',
-		description: `You get notifications when the 'Original Poster' ` +
-		             `appreciates your answer to their question.`,
+		description: 'You get notifications when the \'Original Poster\' ' +
+		             'appreciates your answer to their question.',
 		key: 'opVision'
 	},
 
 	boosterPack: {
 		rep: 50,
-		name: `Booster Pack`,
+		name: 'Booster Pack',
 		icon: '/images/placeholders/rep-50.svg',
 		key: 'boosterPack'
 	},
 
 	smartFilter: {
 		rep: 150,
-		name: `Smart Filter`,
+		name: 'Smart Filter',
 		icon: '/images/placeholders/rep-150.svg',
 		key: 'smartFilter'
 	},
 
 	friendlyNeighbors: {
 		rep: 150,
-		name: `Friendly Neighbors`,
+		name: 'Friendly Neighbors',
 		icon: '/images/placeholders/rep-150-2.svg',
 		key: 'friendlyNeighbors'
 	},
 
 	beastMode: {
 		rep: 400,
-		name: `Beast Mode`,
+		name: 'Beast Mode',
 		icon: '/images/placeholders/rep-400.svg',
 		key: 'beastMode'
 	},
 
 	tagger: {
 		rep: 1000,
-		name: `Tagger`,
+		name: 'Tagger',
 		icon: '/images/placeholders/rep-1k.svg',
 		key: 'tagger'
 	},
 
 	teamPlayer: {
 		rep: 2000,
-		name: `Team Player`,
+		name: 'Team Player',
 		icon: '/images/placeholders/rep-2k.svg',
 		key: 'teamPlayer'
 	},
 
 	amaHost: {
 		rep: 4000,
-		name: `AMA Host`,
+		name: 'AMA Host',
 		icon: '/images/placeholders/rep-4k.svg',
 		key: 'amaHost'
 	}
