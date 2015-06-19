dadceae1c16fba11177a85fbf7b8c26d5cda53ef
AlexKvazos <contact@alexkvazos.com>
2015-06-19 18:17:25 -0500

    Added new icons

--- config/achievementsList.js
+++ config/achievementsList.js
@@ -4,7 +4,8 @@ module.exports = {
 	opVision: {
 		rep: 10,
 		name: 'OP Vision',
-		icon: '/images/placeholders/rep-10.svg',
+		iconGreen: '/images/icons/rep-10.jpg',
+		iconDark: '/images/icons/rep-10-dark.jpg',
 		description: 'You get notifications when the \'Original Poster\' ' +
 		             'appreciates your answer to their question.',
 		key: 'opVision'
@@ -13,49 +14,56 @@ module.exports = {
 	boosterPack: {
 		rep: 50,
 		name: 'Booster Pack',
-		icon: '/images/placeholders/rep-50.svg',
+		iconGreen: '/images/icons/rep-50.jpg',
+		iconDark: '/images/icons/rep-50-dark.jpg',
 		key: 'boosterPack'
 	},
 
 	smartFilter: {
 		rep: 150,
 		name: 'Smart Filter',
-		icon: '/images/placeholders/rep-150.svg',
+		iconGreen: '/images/icons/rep-150.jpg',
+		iconDark: '/images/icons/rep-150-dark.jpg',
 		key: 'smartFilter'
 	},
 
 	friendlyNeighbors: {
 		rep: 150,
 		name: 'Friendly Neighbors',
-		icon: '/images/placeholders/rep-150-2.svg',
+		iconGreen: '/images/icons/rep-150-2.jpg',
+		iconDark: '/images/icons/rep-150-2-dark.jpg',
 		key: 'friendlyNeighbors'
 	},
 
 	beastMode: {
 		rep: 400,
 		name: 'Beast Mode',
-		icon: '/images/placeholders/rep-400.svg',
+		iconGreen: '/images/icons/rep-400.jpg',
+		iconDark: '/images/icons/rep-400-dark.jpg',
 		key: 'beastMode'
 	},
 
 	tagger: {
 		rep: 1000,
 		name: 'Tagger',
-		icon: '/images/placeholders/rep-1k.svg',
+		iconGreen: '/images/icons/rep-1k.jpg',
+		iconDark: '/images/icons/rep-1k-dark.jpg',
 		key: 'tagger'
 	},
 
 	teamPlayer: {
 		rep: 2000,
 		name: 'Team Player',
-		icon: '/images/placeholders/rep-2k.svg',
+		iconGreen: '/images/icons/rep-2k.jpg',
+		iconDark: '/images/icons/rep-2k-dark.jpg',
 		key: 'teamPlayer'
 	},
 
 	amaHost: {
 		rep: 4000,
 		name: 'AMA Host',
-		icon: '/images/placeholders/rep-4k.svg',
+		iconGreen: '/images/icons/rep-4k.jpg',
+		iconDark: '/images/icons/rep-4k-dark.jpg',
 		key: 'amaHost'
 	}
 
