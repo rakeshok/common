46e22a574979b0835fa936b5f70dc1069093c7ca
David Oliveros <dato.oliveros@gmail.com>
2015-09-17 14:14:02 -0500

    Added space validations in sanitizeUrl

--- util/sanitizeUrl.js
+++ util/sanitizeUrl.js
@@ -1,6 +1,11 @@
 
 function sanitizeUrl(string) {
-	if (!string || string.indexOf('.') < 0) return '';
+  string = string ? string.trim() : string;
+
+	if (!string || string.indexOf('.') < 0 || string.indexOf(' ') > -1) {
+    return '';
+  }
+
 	if (string.indexOf('http://') < 0 && string.indexOf('https://') < 0 && string.indexOf('ftp://') < 0) {
 		string = 'http://' + string;
 	}
