d8f6058427a8751018aa72327d10ff8d2eb29f71
David Oliveros <dato.oliveros@gmail.com>
2015-09-17 15:19:42 -0500

    Reset lastIndex in urlRegex after calling isURL

--- util/isURL.js
+++ util/isURL.js
@@ -1,7 +1,10 @@
 var urlRegex = require('../regex/urlRegex');
 
 function isURL(string) {
-	return typeof string === 'string' && string.length ? urlRegex.test(string) : false;
+  if (typeof string !== 'string' || !string.length) return false;
+  var test = urlRegex.test(string);
+  urlRegex.lastIndex = 0;
+	return test;
 }
 
 module.exports = isURL;
